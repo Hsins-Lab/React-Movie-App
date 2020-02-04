d7be463b544c3f88172e91e69332328b5c1fc463
hsins <hsinspeng@gmail.com>
2020-02-04 23:10:57 +0800

    Updates

---
+++ precache-manifest.5ff99fa9f6818acc6dfed40901f4449e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4523fbc5a81b4485333c5c1c4213ba9",
+    "url": "/udemy_React-Movie-App/index.html"
+  },
+  {
+    "revision": "1b9cbc78f762bd201782",
+    "url": "/udemy_React-Movie-App/static/css/main.6acc8b70.chunk.css"
+  },
+  {
+    "revision": "73f30cdf8458693a2bf6",
+    "url": "/udemy_React-Movie-App/static/js/2.fa1cfe66.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/udemy_React-Movie-App/static/js/2.fa1cfe66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b9cbc78f762bd201782",
+    "url": "/udemy_React-Movie-App/static/js/main.49a8ed8f.chunk.js"
+  },
+  {
+    "revision": "b3657835f628cea24ce2",
+    "url": "/udemy_React-Movie-App/static/js/runtime-main.0ec0529f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/udemy_React-Movie-App/precache-manifest.5ff99fa9f6818acc6dfed40901f4449e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/udemy_React-Movie-App/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
